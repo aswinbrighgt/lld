50747d52cf8c5baaf5da22e1ec12cf3dd47c411f
aswinbrightgt <aswinbright46@gmail.com>
2024-02-02 18:22:20 +0530

    change in app adapter

--- src/main/java/designpatterns/adapter/App.java
+++ src/main/java/designpatterns/adapter/App.java
@@ -4,6 +4,8 @@ public class App {
 
     public static void main(String[] args) {
 
+
+        System.out.println();
         Phonepe phonepe = new Phonepe(new IciciBankAdapter());
         phonepe.performBankOperation();
     }
